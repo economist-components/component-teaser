1a2080f42dcbc135e42f705843a003426e8d42b2
Daniel Moll <dan.moll@icloud.com>
2016-05-23 16:05:17 +0100

    feat: slotify the component introducing sub components to enable schema.org `Article` props in a slotifiable way.
    BREAKING CHANGE:
    changes the api completely using sub components as building blocks to plug and play title, fly title, image etc while
    allowing any custom html to be added using JSX. CSS has been stripped down to give control to the parent component.

--- src/index.js
+++ src/index.js
@@ -1,171 +1,285 @@
 import React from 'react';
+import Picture from '@economist/component-picture';
+import classNames from 'classnames';
+import moment from 'moment';
 
-export default class Teaser extends React.Component {
-  static get propTypes() {
-    return {
-      image: React.PropTypes.shape({
-        src: React.PropTypes.string,
-      }),
-      className: React.PropTypes.string,
-      section: React.PropTypes.string,
-      flyTitle: React.PropTypes.string,
-      title: React.PropTypes.string.isRequired,
-      dateTime: React.PropTypes.instanceOf(Date),
-      dateString: React.PropTypes.string,
-      timestampISO: React.PropTypes.string,
-      dateFormat: React.PropTypes.func,
-      text: React.PropTypes.node,
-      extraGroupText: React.PropTypes.node,
-      link: React.PropTypes.shape({
-        href: React.PropTypes.string,
-      }),
-      renderLink: React.PropTypes.func,
-      itemType: React.PropTypes.string,
-      itemProp: React.PropTypes.string,
-    };
+export function TeaserImage({
+  className,
+  sources,
+  alt,
+}) {
+  return (
+    <Picture
+      sources={sources}
+      className={classNames('teaser__image', className)}
+      alt={alt}
+    />
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  TeaserImage.propTypes = {
+    className: React.PropTypes.string,
+    sources: React.PropTypes.arrayOf(
+      React.PropTypes.object,
+    ).isRequired,
+    alt: React.PropTypes.string,
+  };
+}
+
+export function TeaserSection({
+  className,
+  children,
+  meta = false,
+}) {
+  if (meta) {
+    return (
+      <meta itemProp="articleSection" content={children} />
+    );
   }
-  static get defaultProps() {
-    const tenMinutes = 10;
-    return {
-      itemType: 'http://schema.org/Article',
-      itemProp: 'article',
-      dateFormat: (date) => {
-        // Sep 19th 2015, 9:49
-        function addPostFix(day) {
-          const daystr = day.toString();
-          const lastChar = daystr.charAt(daystr.length - 1);
-          let postFix = '';
-          switch (lastChar) {
-            case '1':
-              postFix = 'st';
-              break;
-            case '2':
-              postFix = 'nd';
-              break;
-            case '3':
-              postFix = 'rd';
-              break;
-            default:
-              postFix = 'th';
-              break;
+  return (
+    <div
+      className={classNames('teaser__section', className)}
+      itemProp="articleSection"
+    >
+      {children}
+    </div>
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  TeaserSection.propTypes = {
+    className: React.PropTypes.string,
+    children: React.PropTypes.node,
+    meta: React.PropTypes.bool,
+  };
+}
+
+export function TeaserFlyTitle({
+  className,
+  children,
+  meta = false,
+}) {
+  if (meta) {
+    return (
+      <meta itemProp="alternativeHeadline" content={children} />
+    );
+  }
+  return (
+    <div
+      className={classNames('teaser__flytitle', className)}
+      itemProp="alternativeHeadline"
+    >
+      {children}
+    </div>
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  TeaserFlyTitle.propTypes = {
+    className: React.PropTypes.string,
+    children: React.PropTypes.node,
+    meta: React.PropTypes.bool,
+  };
+}
+
+export function TeaserTitle({
+  className,
+  children,
+  meta = false,
+}) {
+  if (meta) {
+    return (
+      <meta itemProp="headline" content={children} />
+    );
+  }
+  return (
+    <div
+      className={classNames('teaser__title', className)}
+      itemProp="headline"
+    >
+      {children}
+    </div>
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  TeaserTitle.propTypes = {
+    className: React.PropTypes.string,
+    children: React.PropTypes.node,
+    meta: React.PropTypes.bool,
+  };
+}
+
+export function TeaserText({
+  className,
+  children,
+  meta = false,
+}) {
+  if (meta) {
+    return (
+      <meta itemProp="description" content={children} />
+    );
+  }
+  return (
+    <div
+      className={classNames('teaser__text', className)}
+      itemProp="description"
+    >
+      {children}
+    </div>
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  TeaserText.propTypes = {
+    className: React.PropTypes.string,
+    children: React.PropTypes.node,
+    meta: React.PropTypes.bool,
+  };
+}
+
+export function TeaserLink({
+  className,
+  children,
+  href,
+  meta = false,
+}) {
+  if (meta) {
+    return (
+      <div className={classNames('teaser__wrapper', className)}>
+        <link href={href} itemProp="url" />
+        {children}
+      </div>
+    );
+  }
+  return (
+    <a
+      href={href}
+      itemProp="url"
+      className={classNames('teaser__link', className)}
+    >
+      {children}
+    </a>
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  TeaserLink.propTypes = {
+    className: React.PropTypes.string,
+    children: React.PropTypes.node,
+    href: React.PropTypes.string,
+    meta: React.PropTypes.bool,
+  };
+}
+
+export function TeaserPublishDate({
+  className,
+  format = 'MMM Do YYYY, hh:mm:ss',
+  dateTime,
+  meta = false,
+}) {
+  dateTime = new Date(dateTime);
+  const formatedDate = moment(dateTime).format(format);
+  if (meta) {
+    return (
+      <meta content={dateTime} itemProp="datePublished dateModified" />
+    );
+  }
+  return (
+    <time
+      className={classNames('teaser__datetime', className)}
+      itemProp="datePublished dateModified"
+      dateTime={dateTime}
+    >
+      {formatedDate}
+    </time>
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  TeaserPublishDate.propTypes = {
+    className: React.PropTypes.string,
+    dateTime(props, propName, componentName) {
+      if (Number.isNaN(new Date(props[propName]))) {
+        return new Error(`Invalid prop ${ propName } supplied to ${ componentName
+          }. Expected Date got ${ typeof props[propName] }`);
+      }
+      return null;
+    },
+    format: React.PropTypes.string,
+    meta: React.PropTypes.bool,
+  };
+}
+
+export default function Teaser({
+  className,
+  itemType = 'http://schema.org/Article',
+  itemProp = 'article',
+  children,
+  publisher = 'Economist',
+}) {
+  return (
+    <article
+      className={classNames('teaser', className)}
+      itemScope itemType={itemType} itemProp={itemProp}
+      role="article"
+    >
+      <meta itemProp="publisher author" content={publisher} />
+      {children}
+    </article>
+  );
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  Teaser.propTypes = {
+    publisher: React.PropTypes.string,
+    className: React.PropTypes.string,
+    itemType: React.PropTypes.string,
+    itemProp: React.PropTypes.string,
+    children: (props, propName, componentName) => {
+      // not required: TeaserLink, TeaserText, TeaserFlyTitle, TeaserSection
+      // required: TeaserImage, TeaserTitle, TeaserPublishDate
+      let children = [];
+      if (props.children) {
+        if (Array.isArray(props.children)) {
+          let teaserLinkExists = false;
+          props.children.forEach((child, index) => {
+            if (child.type === TeaserLink) {
+              teaserLinkExists = true;
+              children = props.children[index].props.children;
+            }
+          });
+
+          if (teaserLinkExists === false) {
+            children = props.children;
           }
-          return `${ day }${ postFix }`;
+        } else {
+          children = props.children.props.children;
         }
-        const shortMonthList = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
-        let minutes = date.getMinutes() < tenMinutes ? '0' : '';
-        minutes += date.getMinutes();
-        return `${ shortMonthList[date.getMonth()] }
-                ${ addPostFix(date.getDate()) }
-                ${ date.getFullYear() },
-                ${ date.getHours() }:${ minutes }`;
-      },
-    };
-  }
-  wrapGroupsInLink(groups) {
-    if (this.props.link) {
-      const LinkComponent = this.props.renderLink || 'a';
-      return (
-        <LinkComponent {...this.props.link}
-          className="teaser__link"
-          itemProp="url"
-        >{groups}</LinkComponent>);
-    }
-    return (
-      <div className="teaser__wrapper">
-        {groups}
-      </div>
-    );
-  }
-  render() {
-    const teaserContent = [];
-    const groups = [];
-    const imageSrc = this.props.image && this.props.image.src;
-    let imageClasses = [ 'teaser__group-image' ];
-    if (!imageSrc) {
-      imageClasses = imageClasses.concat([ 'teaser__group-image--empty' ]);
-    }
-    const image = imageSrc ?
-      (<img {...this.props.image} itemProp="image" className="teaser__img" />) :
-      null;
-    groups.push((
-      <div className={imageClasses.join(' ')} key="group-image">
-        {image}
-      </div>));
-    if (this.props.section) {
-      teaserContent.push((
-        <h3
-          className="teaser__section"
-          itemProp="section"
-          key="section"
-        >{this.props.section}</h3>
-      ));
-    }
-    if (this.props.flyTitle) {
-      teaserContent.push((
-        <h2
-          className="teaser__flytitle"
-          itemProp="alternativeHeadline"
-          key="flytitle"
-        >{this.props.flyTitle}</h2>
-      ));
-    }
-    if (this.props.title) {
-      teaserContent.push((
-        <h1
-          className="teaser__title"
-          itemProp="headline"
-          key="title"
-        >{this.props.title}</h1>));
-    }
-    if (this.props.dateTime) {
-      teaserContent.push((
-        <time
-          className="teaser__datetime"
-          itemProp="dateCreated"
-          dateTime={this.props.dateTime}
-          key="datetime"
-        >{this.props.dateFormat(this.props.dateTime)}</time>));
-    }
-    if (this.props.dateString && this.props.timestampISO) {
-      teaserContent.push((
-        <time
-          className="teaser__datetime"
-          itemProp="dateCreated"
-          dateTime={this.props.timestampISO}
-          key="datetime"
-        >{this.props.dateString}</time>));
-    }
-    if (this.props.text) {
-      teaserContent.push((
-        <div
-          className="teaser__text"
-          itemProp="description"
-          key="text"
-        >
-          {this.props.text}
-        </div>
-      ));
-    }
-    groups.push((
-      <div className="teaser__group-text"
-        key="grouptext"
-      >
-        {teaserContent}
-        {this.props.extraGroupText}
-      </div>
-    ));
-    const content = this.wrapGroupsInLink(groups);
-    let classNames = [ 'teaser' ];
-    if (this.props.className) {
-      classNames = classNames.concat(this.props.className);
-    }
-    return (
-      <article
-        className={classNames.join(' ')}
-        itemScope itemType={this.props.itemType} itemProp={this.props.itemProp}
-        role="article"
-      >{content}</article>
-    );
-  }
+      }
+
+      let teaserImageExists = false;
+      let teaserPublishDateExists = false;
+      let teaserTitleExists = false;
+      children.forEach((child) => {
+        teaserImageExists = teaserImageExists || child.type === TeaserImage;
+        teaserPublishDateExists = teaserPublishDateExists || child.type === TeaserPublishDate;
+        teaserTitleExists = teaserTitleExists || child.type === TeaserTitle;
+      });
+
+      if (teaserImageExists === false) {
+        return new Error(`Invalid component TeaserImage not specified in
+          ${ propName } for ${ componentName }.`);
+      } else if (teaserPublishDateExists === false) {
+        return new Error(`Invalid component TeaserPublishDate not specified in
+          ${ propName } for ${ componentName }.`);
+      } else if (teaserTitleExists === false) {
+        return new Error(`Invalid component TeaserTitle not specified in
+          ${ propName } for ${ componentName }.`);
+      }
+
+      return null;
+    },
+  };
 }
